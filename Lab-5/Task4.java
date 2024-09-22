2d17c6a4917ad5a2efce7d11ce91c70950f3608d
Bilal Ahmed <150439745+Bilal12342@users.noreply.github.com>
2024-09-22 13:12:25 +0500

    Upload Lab-5 Task04

--- Lab-5/Task4.java
+++ Lab-5/Task4.java
@@ -1,5 +1,75 @@
-public class Task4 {
-    public static void main(String[] args) {
-
+class MyStack {
+    private int maxSize;
+    private char[] stackArray;
+    private int top;
+    public MyStack(int size) {
+        maxSize = size;
+        stackArray = new char[maxSize];
+        top = -1; 
+    }
+    public void push(char value) {
+        stackArray[++top] = value;
+    }
+    public char pop() {
+        return stackArray[top--];
+    }
+    public char peek() {
+        return stackArray[top];
+    }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (top == maxSize - 1);
     }
 }
+public class Task4 {
+    private static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+    public static String infixToPostfix(String expression) {
+        StringBuilder result = new StringBuilder();
+        MyStack stack = new MyStack(expression.length());
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                result.append(c);
+            }
+            else if (c == '(') {
+                stack.push(c);
+            }
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop(); 
+            }
+            else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String infixExpression = "A+B*C+D";
+        System.out.println("Infix Expression: " + infixExpression);
+        System.out.println("Postfix Expression: " + infixToPostfix(infixExpression));
+    }
+}
+
