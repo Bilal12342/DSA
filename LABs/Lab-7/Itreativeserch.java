446ef0802d1b920013a14cf59db4108813b296b2
Bilal Ahmed <150439745+Bilal12342@users.noreply.github.com>
2024-10-09 21:36:05 +0500

    code set

--- LABs/Lab-7/Itreativeserch.java
+++ LABs/Lab-7/Itreativeserch.java
@@ -1,9 +1,4 @@
-
-    
-    import java.util.Random;
-import java.util.Scanner;
 public class Itreativeserch {
-
     public static int iterativeSearch(int[] arr, int searchValue) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == searchValue) {
